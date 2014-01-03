b6be16ab4e6bd4bce6f5e72d80a85bf2181f0676
squeetus <burlinsond2@gmail.com>
2014-01-03 00:38:24 -0500

    Update main.php

--- main.php
+++ main.php
@@ -1,7 +1,13 @@
 <?php
-$Units = 'cup cups can g gram grams kg kilogram L l liter lb pound mL ml milliliter oz ounce pt pint t tsp teaspoon T TB Tbl Tbsp tablespoon head inches inch batch clove cloves bunch ';
-
+$Units = "to taste as box needed cup cups can g gram grams kg kilogram L l liter lb pound mL ml milliliter oz. oz ounce pt pint t tsp teaspoon T TB Tbl Tbsp tablespoon head inches inch batch clove cloves bunch";
+$Fractions = array(
+    "&frac12;" => .5,
+    "&frac14;" => .25,
+    "&frac13;" => .333,
+    "&frac34;" => .75
+    );
 $IngredientList = array();
+$RecipeList = array();
 
 // Class structure for an ingredient
 class Ingredient {
@@ -11,8 +17,32 @@ class Ingredient {
     public $measure = "";
     
     public function display() {
-        echo $this->amount . "\t\t" . $this->measure . "\t\t" . $this->item . "\t\t" . $this->cost . "\n";
+        echo "<td>" . $this->amount . "</td><td>" . $this->measure . "</td><td>" . $this->item . "</td><td>$" . $this->cost . "</td>";
     }
+}
+
+// Checks to see if the value is a fraction
+function is_fraction($x) {
+    global $Fractions;
+    foreach($Fractions as $key => $val) {
+        //echo $x . " " . $key . " " . $val . "<br />";
+        
+        if($x == $key) {
+            //echo $x . " " . $key . " " . $val . "<br />";
+            return true;
+        }
+    }
+    return false;
+}
+
+// Returns the decimal value for an html fraction
+function get_fraction($x) {
+    global $Fractions;
+    foreach($Fractions as $key => $val) {
+        if($x == $key)
+            return $val;
+    }
+    return false;
 }
 
 // Finds ingredients from the given string
@@ -22,58 +52,137 @@ function parseForIngredients($content) {
     global $Units;
     global $IngredientList;
     
+    
     while($sentinel) {
-        
+        $item = "";
+        $measure = "";
+        $word = "";
         $ingredient = new Ingredient();
         
         $startpos = strpos($content, '<li class="ingredient" itemprop="ingredients">');
-        $startpos += 46;
+        if($startpos !== false)
+            $startpos += 46;
         $content = substr($content, $startpos);
         $endpos = strpos($content, '</li>');
         $line = substr($content, 0, $endpos);
         
         $line = explode(" ", $line);
         
-        if(is_numeric($line[0]))
-            $ingredient->amount = (int) $line[0];
-         
+        $word = $line[0];
+        
+        if(is_numeric($word)) {
+            $ingredient->amount = (int) $word;
+        } else if(is_fraction($word)) {
+            $ingredient->amount = (double) get_fraction($word);
+        } else {
+            $ingredient->amount = "-";
+        }
+        
         $i = count($line);
         
-        $cost = substr($line[$i],1);
+        $cost = substr($line[$i-1],1);
+        
         if(is_numeric($cost))
             $ingredient->cost = (double) $cost;
         
-        $item = "";
-        
-        for($j=1;$j<$i;$j++)
-            if(strpos($Units, $line[$j]) !== false) {
-                $ingredient->measure += " " . $line[$j];
-            } else {
-                $item = $item . " " . $line[$j];    
+        for($j=0;$j<$i-1;$j++) {
+            $word = $line[$j];
+            if(!(is_numeric($word) || is_fraction($word))) 
+            {
+                if((strpos($Units, $word) !== false) || (substr($word, 0,1) === '(') || (substr($word, -1) === ')')){
+                    $measure = $measure . $word . " ";
+                } else {
+                    $item = $item . " " . $word; 
+                }
             }
+        }
+        
+        if(empty($measure))
+            $ingredient->measure = "-";
+        else
+            $ingredient->measure = $measure;
             
         $ingredient->item = $item;
-           
-        $IngredientList[$count] = &$ingredient;
+        
+        $inList = false;
+        
+        foreach($IngredientList as $struct) {
+            if($ingredient->item == $struct->item) {
+                $inList = true;
+                break;
+            }
+        }
+          
+        if(!$inList) 
+            $IngredientList[] = $ingredient;
+        
         if(strpos($content,'<li class="ingredient" itemprop="ingredients">') === false)
             $sentinel = false;
+            
         $count += 1;
     }
 }
+
+// Prints out contents of ingredient array to file
+function writeIngredientsToFile(){
+    global $IngredientList;   
+    $file = 'datafile.txt';
+    $current = file_get_contents($file);
+
+    foreach($IngredientList as $ingredient) {
+        $current .= $ingredient->amount . " " . $ingredient->measure . " " . $ingredient->item . " " . $ingredient->cost . "\n";
+    }
+    
+    file_put_contents($file, $current);
+}
+
+
+
 
 // Prints out contents of ingredient array
 function printIngredients() {
     global $IngredientList;    
 
-    echo "Amount\t\tMeasurement\t\tItem\t\tCost\n\n";
-
-    foreach($IngredientList as &$ingredient) {
-        $ingredient->display();
+    echo "<table cellspacing='10'><tr align='right'><td>Amount</td><td>Measurement</td><td>Item</td><td>Cost</td></tr>";
+    
+    foreach($IngredientList as $ingredient) {
+        echo "<tr>" . $ingredient->display() . "</tr>";
     }
 
+    echo "</table>";
 }
 
-// Point of entry
+// Prints all the current recipes
+function printRecipeList() {
+    global $RecipeList;
+    
+    echo "<ul>";
+    foreach($RecipeList as $recipe) {
+        echo "<li><a href='" . $recipe . "'>" . $recipe . "</a></li>";
+    }
+    echo "</ul>";
+    
+}
+    
+function loadFromFile() {
+    global $IngredientList;   
+    $file = 'datafile.txt';
+    $current = file_get_contents($file);
+
+    
+    
+    file_put_contents($file, $current);
+}
+
+/************************************************************
+ *                                                          *
+ *                  Point of Entry                          *
+ *                                                          *
+ * **********************************************************/
+loadFromFile();
+
+echo "Shopping List<br /><br />";
+
 if ( ! empty($_POST['Recipe_Input_Box'])){
     
     $url = $_POST['Recipe_Input_Box'];
@@ -87,10 +196,14 @@ if ( ! empty($_POST['Recipe_Input_Box'])){
     $url = $_POST['Recipe_Input_Box'];
     $content = file_get_contents($url);
     
+    $RecipeList[] = $url;
+    printRecipeList();
+    
     parseForIngredients($content);
     
-    //printIngredients();
-    var_dump ($IngredientList);
+    printIngredients();
+    writeIngredientsToFile();
+   
 } else {
     echo "
     <form name='form' method='post' action='main.php'>
@@ -98,8 +211,7 @@ if ( ! empty($_POST['Recipe_Input_Box'])){
     <input type='submit' name='submit' value='Submit'></form>
     ";
     
+    printRecipeList();
 }
 
-
 ?>
-
